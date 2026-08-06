ee296c37ca4a8ed986d5e153b047e0b00d543408
@khalilmohamed798 <khalilmohamed798@gmail.com>
2026-08-06 13:51:00 +0100

    firebase conf :Only create the bean when the property exists

--- src/main/java/com/bib/app/Confi/FirebaseConfig.java
+++ src/main/java/com/bib/app/Confi/FirebaseConfig.java
@@ -4,12 +4,15 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
+@ConditionalOnProperty(name = "firebase.credentials.path")
 public class FirebaseConfig {
 
     @PostConstruct
