9af500bb686404e576510939ba4ce3d5664a2c12
@khalilmohamed798 <khalilmohamed798@gmail.com>
2026-08-20 13:30:14 +0100

    manage vault servicemonitor with terraform

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -3,3 +3,13 @@ resource "kubernetes_namespace" "vault" {
     name = "vault"
   }
 }
+
+resource "kubernetes_manifest" "vault_servicemonitor" {
+  manifest = yamldecode(file("${path.module}/vault-servicemonitor.yaml"))
+
+  depends_on = [
+    kubernetes_namespace.vault,
+    kubernetes_namespace.monitoring,
+    helm_release.kube_prometheus_stack
+  ]
+}
