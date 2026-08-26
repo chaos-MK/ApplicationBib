d0ed05390a41ae01e02dc5642a1b96003395d0e2
@khalilmohamed798 <khalilmohamed798@gmail.com>
2026-08-26 22:07:21 +0100

    chore(frontend): update frontend image [skip ci]

--- terraform/ritual-growth-ui-deployment.tf
+++ terraform/ritual-growth-ui-deployment.tf
@@ -34,7 +34,7 @@ resource "kubernetes_deployment_v1" "ritual_growth_ui" {
 
         container {
           name              = "ritual-growth-ui"
-          image             = "registry.gitlab.com/khalilmohamed798/ritual-growth-ui:7acddb80"
+          image             = "registry.gitlab.com/khalilmohamed798/ritual-growth-ui:9bee2edd"
           image_pull_policy = "Always"
 
           port {
