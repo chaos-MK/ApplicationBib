b90f27177f3be7fa5923d4127dbd043759aed7a3
@khalilmohamed798 <khalilmohamed798@gmail.com>
2026-08-26 22:06:56 +0100

    feat(monitoring): manage Grafana dashboards with Terraform [skip ci]

--- terraform/grafana-dashboards.tf
+++ terraform/grafana-dashboards.tf
@@ -7,3 +7,22 @@ resource "kubernetes_manifest" "vault_dashboard" {
     helm_release.kube_prometheus_stack
   ]
 }
+
+resource "kubernetes_config_map" "ritual_growth_ui_dashboard" {
+  metadata {
+    name      = "ritual-growth-ui-dashboard"
+    namespace = "monitoring"
+
+    labels = {
+      grafana_dashboard = "1"
+    }
+  }
+
+  data = {
+    "ritual-growth-ui.json" = file("${path.module}/../k8s/monitoring/dashboards/ritual-growth-ui-dashboard.json")
+  }
+
+  depends_on = [
+    helm_release.kube_prometheus_stack
+  ]
+}
